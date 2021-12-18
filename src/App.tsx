58a783942d92f79f1fc027ff40c85f05a33f89be
Twofold-One <evan_geras@mail.ru>
2021-12-18 23:33:03 +0700

    data types rendering in progress

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,9 @@ function App() {
             <div className="Main">
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/shop" element={<Shop />}>
+                        <Route path=":shopName" element={<Shop />} />
+                    </Route>
                     <Route path="/cart" element={<Cart />} />
                 </Routes>
             </div>
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,11 +1,54 @@
 import styles from './styles/Shop.module.scss';
 import { data } from '../data/guitars_data';
 import Card from '../components/Card';
+import { Link, useParams } from 'react-router-dom';
+import { capitalizeFirstLetter } from '../utility_functions/capitalize_first_letter';
+import React from 'react';
 
 const Shop = () => {
+    let params = useParams();
+    const dataKeys = Object.keys(data);
+
+    const handleGuitarTypeClick = (e: React.MouseEvent) => {
+        console.log(params);
+    };
+
+    const renderGuitars = () => {
+        // dataKeys.map((key: keyof typeof data) => {
+        //     return <h1>{data[key]}</h1>;
+        // });
+
+        return data.electric.map((item) => {
+            return (
+                <Card
+                    id={item.id}
+                    key={item.id}
+                    name={item.name}
+                    price={item.price}
+                    image={item.image}
+                    description={item.description}
+                />
+            );
+        });
+    };
+
     return (
         <div className={styles.Shop}>
-            {data.electric.map((item) => {
+            <div>
+                <Link to={`/shop`}>
+                    <h2>All guitars</h2>
+                </Link>
+                {Object.keys(data).map((key) => {
+                    return (
+                        <Link key={key} to={`/shop/${key}`}>
+                            <h2 onClick={handleGuitarTypeClick}>
+                                {capitalizeFirstLetter(key)}
+                            </h2>
+                        </Link>
+                    );
+                })}
+            </div>
+            {/* {data.electric.map((item) => {
                 return (
                     <Card
                         id={item.id}
@@ -16,7 +59,8 @@ const Shop = () => {
                         description={item.description}
                     />
                 );
-            })}
+            })} */}
+            {renderGuitars()}
         </div>
     );
 };
